7099124e9eb0d2d132c8c305d5394586118dae4f
asyncins <asyncins@aliyun.com>
2019-05-19 15:31:46 +0800

    与服务主机连同

---
+++ server.py
@@ -0,0 +1,27 @@
+from tornado import ioloop
+import tornado.web
+from custom import secret_websocket_connect
+from settings import *
+
+
+async def register():
+    """ lite 服务器启动时自动注册到主服务器 """
+    swc = await secret_websocket_connect('ws://%s/api/v1/register' % SERVER_MASTER)
+    resp = await swc.read_message()
+    print(resp)
+    while True:
+        # 接收主服务器推送的消息
+        message = await swc.read_message()
+        if message:
+            # 此处可以根据消息作出不同的响应
+            print(message)
+            pass
+
+
+if __name__ == "__main__":
+
+    app = tornado.web.Application(debug=False)
+    app.listen(PORT)
+    loop = ioloop.IOLoop.current()
+    loop.run_sync(register)
+    loop.start()
