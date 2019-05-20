7f5531cb182132b343e839d2211b6826fd46c492
asyncins <asyncins@aliyun.com>
2019-05-20 18:11:28 +0800

    socket2restful

--- server.py
+++ server.py
@@ -1,23 +1,18 @@
 """启动文件
 并实现握手和通信功能"""
+import json
 from tornado import ioloop
 import tornado.web
-from custom import secret_websocket_connect
+from tornado.httpclient import AsyncHTTPClient
 from settings import *
 
 
 async def register():
-    """ lite 服务器启动时自动注册到主服务器 """
-    swc = await secret_websocket_connect('ws://%s/api/v1/register' % SERVER_MASTER)
-    resp = await swc.read_message()
-    print(resp)
-    while True:
-        # 接收主服务器推送的消息
-        message = await swc.read_message()
-        if message:
-            # 此处可以根据消息作出不同的响应
-            print(message)
-            pass
+    """ lite服务器启动时自动注册到主服务器 """
+    url = 'http://' + SERVER_MASTER + '/api/v1/reg' + "?secret=%s&port=%s" % (SERVER_SECRET_KEY, PORT)
+    client = AsyncHTTPClient()
+    resp = await client.fetch(url)
+    print(json.loads(resp.body))
 
 
 if __name__ == "__main__":
